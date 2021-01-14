bda26f74f0fe9b33c66b41336f9ea7500b8e9562
Long minh <longminh207@gmail.com>
2021-01-14 16:30:37 +0700

    bai tap ngay 4

---
+++ D04/MinhLong/number.js
@@ -0,0 +1,16 @@
+let nhap = prompt("Nhap so: ");
+
+count(nhap)
+
+function count(nhap){
+let donvi = ("không","một","hai","ba","bốn","năm","sáu","bảy","tám","chín");
+let chuc = ("mười","mươi","tư","lăm");
+let demChuoi = 1;
+let docSo = "";
+
+for (let i = nhap.length; i >=0; i-- ){
+    let docso = parseInt(donvi.charAt(donvi.length-i))
+}
+}
+
+alert(nhap);
